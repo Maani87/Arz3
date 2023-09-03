ab730ab9d766b0053a74ef4657e7724ada5384eb
Maani87 <maniplays2008@gmail.com>
2023-09-03 11:53:12 +0330

    initial commit

---
+++ Arz/web/models.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(
+    User,
+    models.SET_NULL,
+    blank=True,
+    null=True,)
+    def __unicode__(self):
+        return "{}-{}".format(self.date , self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(
+    User,
+    models.SET_NULL,
+    blank=True,
+    null=True,)
+    def __unicode__(self):
+        return "{}-{}".format(self.date , self.amount)
