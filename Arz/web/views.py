bef0c8bfecaf3d1a7ad8c01ac7a24d4af885a49d
Maani87 <mania8788@gmail.com>
2023-09-04 23:39:18 +0330

    income models

--- Arz/web/views.py
+++ Arz/web/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 def submit_expense(request):
     """user submits an expense"""
-    print ("we are here")
+
+    print ("im in submit expense")
+    print (request.POST)
+
+    return JsonResponse({
+        "status": "ok", 
+    },encoder=JASONEncoder)
