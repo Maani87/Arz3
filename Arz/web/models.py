3960bd7b2bdb5cda98d973d33882e61af32361ea
Maani87 <mania8788@gmail.com>
2023-09-04 12:47:10 +0330

    income models

--- Arz/web/models.py
+++ Arz/web/models.py
@@ -3,28 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+class Token(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    token = models.CharField(max_length=60)
 
-# Create your models here.
+
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(
-    User,
-    models.SET_NULL,
-    blank=True,
-    null=True,)
-    def __unicode__(self):
-        return "{}-{}".format(self.date , self.amount)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(
-    User,
-    models.SET_NULL,
-    blank=True,
-    null=True,)
-    def __unicode__(self):
-        return "{}-{}".format(self.date , self.amount)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
--- Arz/web/urls.py
+++ Arz/web/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-url(r"^submit/expense/$" , views.submit_expense , name= "submit_expense")
+path("submit/expense/" , views.submit_expense , name= "submit_expense")
 ]
